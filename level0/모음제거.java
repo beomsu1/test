15bc9f092099abedf52d4f999c11eb154755bc43
beomsu1 <beumsu1221@gmail.com>
2023-10-14 18:37:00 +0900

    모음제거

--- level0/모음제거.java
+++ level0/모음제거.java
@@ -34,4 +34,17 @@ class Solution {
 
         return answer;
     }
+    
+}
+
+
+// replaceAll 사용
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+
+        answer = my_string.replaceAll("[aeiou]", "");
+
+        return answer;
+    }
 }
